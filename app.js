4b6eb62584972d2c694ec8762c5d8ce4edde8fcf
Juan Manuel Hurtado isaza <166757571+Sickcop@users.noreply.github.com>
2024-07-09 18:58:48 -0500

    adding-movement

--- app.js
+++ app.js
@@ -8,21 +8,23 @@ let maxTable = 10
 let minTable = 1
 let posy = 6
 let posx = 6
+let active = false
 
-window.addEventListener('keyup',(e)=>{
+window.addEventListener('keyup', (e)=> {
+
 
     if (e.code == 'ArrowUp'){
 
       let intervalId = setInterval(()=>{
         if (posy <= minTable) { 
-        console.log(posy)
-        clearInterval(intervalId)
-      }
-      else {
-        console.log(posy)
-        posy --
-        snake.style.gridArea = `${posy} / ${posx}`
-      }
+          console.log(posy)
+          clearInterval(intervalId)
+        }
+        else {
+          console.log(posy)
+          posy --
+          snake.style.gridArea = `${posy} / ${posx}`
+        }
       }, 200)
 
         
@@ -32,14 +34,14 @@ window.addEventListener('keyup',(e)=>{
       
       let intervalId = setInterval(()=>{
         if (posy > maxTable) { 
-        console.log(posy)
-        clearInterval(intervalId)
-      }
+          console.log(posy)
+          clearInterval(intervalId)
+        }
       else {
         console.log(posy)
-        posy ++
-        snake.style.gridArea = `${posy} / ${posx}`
-      }
+          posy ++
+          snake.style.gridArea = `${posy} / ${posx}`
+        }
       }, 200)
 
     }
@@ -48,14 +50,14 @@ window.addEventListener('keyup',(e)=>{
 
         let intervalId = setInterval(()=>{
           if (posx > maxTable) { 
-          console.log(posx)
-          clearInterval(intervalId)
-        }
-        else {
-          console.log(posx)
-          posx ++
-          snake.style.gridArea = `${posy} / ${posx}`
-        }
+            console.log(posx)
+            clearInterval(intervalId)
+          }
+          else {
+            console.log(posx)
+            posx ++
+            snake.style.gridArea = `${posy} / ${posx}`
+          }
         }, 200)
 
     }
@@ -64,17 +66,15 @@ window.addEventListener('keyup',(e)=>{
 
       let intervalId = setInterval(()=>{
         if (posx <= minTable) { 
-        console.log(posx)
-        clearInterval(intervalId)
-      }
-      else {
-        console.log(posx)
-        posx --
-        snake.style.gridArea = `${posy} / ${posx}`
-      }
+          console.log(posx)
+          clearInterval(intervalId)
+        }
+        else {
+          console.log(posx)
+          posx --
+          snake.style.gridArea = `${posy} / ${posx}`
+        }
       }, 200)
 
     }
-    
-    ;
 })
